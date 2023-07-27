f3a3135d5c5c72af0454beef1d8c1706fff57aa1
Ankit Sharma <an2ki4t5s@gmail.com>
2023-07-27 20:01:56 +0530

    updated index file location

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,17 @@
 import express from "express";
-import cors from "cors";
 import dotenv from 'dotenv';
-import connectDB from "./database/db.js";
 import DBconnect from "./Routing/AuthRoutes.js";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(cors({origin: true}));
+
+
 const PORT = process.env.PORT || 2023;
 
 DBconnect();
 
 
-app.use("/auth",authRoutes);
+
 
 app.listen(PORT, () => console.log(`Server Started at: ${PORT}`));
